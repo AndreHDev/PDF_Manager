e15b44474e7d43b61738048110e6b8d6ceac3091
Andre Himmelspach <andre.himmelspach@tapdo.io>
2025-02-19 12:15:20 +0100

    feat: change API to REST specification

--- backend/app/backend.py
+++ backend/app/backend.py
@@ -5,14 +5,25 @@ from io import BytesIO
 from typing import List
 import uuid
 import shutil
-from schemas import Page
+from schemas import Page, Pdf
 from fastapi import UploadFile
 import os
  
 
 class PDF_Model():
+    def __init__(self):
+        self._pdfs = List[Pdf]
+    
+    def get_pdf(self, pdf_id: str) -> Page:
+        for pdf in self._pdfs:
+            if pdf.file_id == pdf_id:
+                return pdf
+        return None
+    
+    def add_pdf(self, pdf: Page):
+        self._pdfs.append(pdf)
 
-    def add_pdf_file(self, file: UploadFile) -> List[Page]:
+    def add_pdf_file(self, file: UploadFile) -> str: #TODO better name?
         """
         Add a PDF file to the backend and extract all pages from it. Save the file temporarily in /temp.
 
@@ -22,12 +33,11 @@ class PDF_Model():
         Returns:
             List[Page]: A list of page objects.
         """
-        response_pages:List[Page] = []
-
+        pages:List[Page] = []
+        file_id = str(uuid.uuid4())
         try:
-            file_id = str(uuid.uuid4())
             # Save the file temporarily
-            file_path = f"temp/{file_id}.pdf"
+            file_path = self.get_pdf_path(file_id)
             with open(file_path, "wb") as buffer:
                 shutil.copyfileobj(file.file, buffer)
                 print(f"Saved file to {file_path}")
@@ -41,13 +51,35 @@ class PDF_Model():
             # Create page for response
             reader = PdfReader(file_path)
             for i in range(len(reader.pages)):
-                new_page = Page(file_id=file_id, page_number=i + 1, thumbnail=thumbnails[i])
-                response_pages.append(new_page)
+                new_page = Page(page_number=i + 1, thumbnail=thumbnails[i])
+                pages.append(new_page)
 
         except Exception as e:
             raise Exception(f"Error processing PDF file: {str(e)}")
+        
+        self.add_pdf(Pdf(file_id=file_id, pages=pages))
 
-        return response_pages
+        return file_id
+    
+
+    def get_pdf_pages(self, pdf_id: str) -> List[Page]:
+        """
+        Get all pages for a specific PDF file.
+
+        Args:
+            pdf_id (str): Id of the PDF file.
+
+        Returns:
+            List[Page]: List of Page objects.
+
+        Raises:
+            FileNotFoundError: Raised if the file is not found.
+        """
+        pdf = self.get_pdf(pdf_id)
+        if not pdf:
+            raise FileNotFoundError
+        
+        return pdf.pages
 
 
     def get_thumbnails(self, file_path: str) -> List[str]:
@@ -73,16 +105,15 @@ class PDF_Model():
             raise Exception
 
     
-    def merge_selected_pages(self, pages: List[Page], output_file_path="merged_output.pdf") -> str:
+    def merge_selected_pages(self, pages: List[Page]) -> str:
         """
         Merge a list of pages into a single PDF file. Only pages with checked=True will be merged.
 
         Args:
             pages (List[Page]): List of pages to merge.
-            output_file_path (str, optional): Path to the final merged file. Defaults to "merged_output.pdf".
 
         Returns:
-            str: Path to the merged file.
+            str: The id of the merged file.
 
         Raises:
             FileNotFoundError: Raised if a file is not found.
@@ -99,8 +130,8 @@ class PDF_Model():
                 continue
             
             try:
-                new_page = self.get_page_in_pdf_format(page.file_id, page.page_number)
-                writer.add_page(new_page)
+                pdf_page = self.get_page_in_pdf_format(page.file_id, page.page_number)
+                writer.add_page(pdf_page)
             except FileNotFoundError:
                 raise FileNotFoundError(f"File {page.file_id} not found.") 
             except IndexError:
@@ -110,12 +141,15 @@ class PDF_Model():
 
         if not writer.pages:
             raise ValueError("No pages were selected for merging.")
+        
+        file_id = str(uuid.uuid4())
+        output_file_path = self.get_pdf_path(file_id)
 
         # Write the merged file
         with open(output_file_path, "wb") as f:
             writer.write(f)
 
-        return output_file_path
+        return file_id
     
     
     def get_page_in_pdf_format(self, file_id: str, page_number: int) -> PdfReader:
@@ -133,8 +167,9 @@ class PDF_Model():
             FileNotFoundError: Raised if the file is not found.
             IndexError: Raised if the page is not found in the file.
         """
+        file_path = self.get_pdf_path(file_id)
         try:
-            reader = PdfReader(f"temp/{file_id}.pdf")
+            reader = PdfReader(file_path)
         except FileNotFoundError:
             raise FileNotFoundError
         
@@ -145,13 +180,6 @@ class PDF_Model():
         
         return page
     
-    def delete_merged_file(self, file_path="merged_output.pdf"):
-        # Delete merged file
-        try:
-            os.remove(file_path)
-        except FileNotFoundError:
-            print("Attempted to delete a File thats already deleted.")
-            pass
 
     def clean_up(self):
         # Clean up all pdf files in temp folder
@@ -161,7 +189,9 @@ class PDF_Model():
                     os.remove(f"temp/{file}")
         except Exception as e:
             raise Exception(f"Error cleaning up temp folder: {str(e)}")
-        
-        self.delete_merged_file()
+
+    
+    def get_pdf_path(self, pdf_id: str) -> str:
+        return f"temp/{pdf_id}.pdf"
 
 pdf_model_instance = PDF_Model()
--- backend/app/router.py
+++ backend/app/router.py
@@ -8,8 +8,8 @@ import os
 
 fileRouter = APIRouter()
 
-@fileRouter.post("/merge")
-async def merge_pdfs(request: List[Page], background_tasks: BackgroundTasks):
+@fileRouter.post("/pages/merge", response_model=dict)
+async def merge_pdfs(request: List[Page]):
     """
     Merge passed page objects into a single PDF file. Only pages with checked=True will be merged.
 
@@ -17,12 +17,12 @@ async def merge_pdfs(request: List[Page], background_tasks: BackgroundTasks):
         request (List[Page]): List of Page objects to merge.
 
     Returns:
-        FileResponse: Merged PDF file.
+        dict: A dictionary containing the file_id of the merged PDF.
     """
     print("Received request to merge PDFs!")
 
     try:
-        output_file = pdf_model_instance.merge_selected_pages(request)
+        merged_file_id = pdf_model_instance.merge_selected_pages(request)
     except ValueError as ve: # No page selected for merging
         raise HTTPException(status_code=400, detail=str(ve))
     except FileNotFoundError as fnf: # File not found
@@ -31,45 +31,88 @@ async def merge_pdfs(request: List[Page], background_tasks: BackgroundTasks):
         raise HTTPException(status_code=404, detail=str(ie))
     except Exception as e: # Everything else
         raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
-    
-
-    output_file = "merged_output.pdf"
-    if not os.path.exists(output_file):
-        raise HTTPException(status_code=404,
-        detail="Merged PDF file not found, something went wrong while tryging to merge the files.")
-
-    background_tasks.add_task(pdf_model_instance.delete_merged_file, output_file)
-
-    response = FileResponse(output_file)
-
-    return response
 
 
-@fileRouter.post("/upload", response_model=List[Page])
-async def upload_file(file: UploadFile = File(...)):
+    return {"file_id": merged_file_id}
+
+
+@fileRouter.post("/pdfs", response_model=dict)
+async def upload_pdf(file: UploadFile = File(...)):
     """
-    Upload a PDF file to get all pages from that file.
+    Uploads a PDF and returns its unique file_id.
 
     Args:
-        file (UploadFile): The PDF file to be uploaded.
+        file (UploadFile, optional): The PDF file to upload.
 
     Returns:
-        List[Page]: A list of page objects. extracted from the uploaded PDF file.
+        dict: A dictionary containing the file_id of the uploaded PDF.
     """
     print("Received request to upload file!")
 
     try:
-        pages = pdf_model_instance.add_pdf_file(file)
+        pdf_id = pdf_model_instance.add_pdf_file(file)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
+    return {"pdf_id": pdf_id}
+
+
+@fileRouter.get("/pdfs/{pdf_id}/pages", response_model=List[Page])
+async def get_pdf_pages(pdf_id: str):
+    """
+    Returns a list of Page objects for a given PDF.
+
+    Args:
+        pdf_id (str): The file_id of the PDF to fetch pages for.
+
+    Returns:
+        List[Page]: A list of Page objects for the given PDF.
+    """
+    print(f"Fetching pages for PDF: {pdf_id}")
+
+    try:
+        pages = pdf_model_instance.get_pdf_pages(pdf_id)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="PDF file not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
     return pages
 
-@fileRouter.get("/clean-up")
-async def clean_up():
-    print("Received request to clean up!")
+
+@fileRouter.get("/pdfs/{pdf_id}/download")
+async def download_pdf(pdf_id: str):
+    """
+    Gets a PDf file for download by its file_id
+    Args:
+        pdf_id (str): The file_id of the PDF to download.
+
+    Returns:
+        FileResponse: The PDF file for download.
+    """
+    print(f"Received request to download PDF: {pdf_id}")
+
+    pdf_path = pdf_model_instance.get_pdf_path(pdf_id)
+
+    if not os.path.exists(pdf_path):
+        raise HTTPException(status_code=404, detail="PDF file not found")
+
+    return FileResponse(pdf_path, filename=f"{pdf_id}.pdf")
+
+
+@fileRouter.delete("/pdfs")
+async def clean_up_temp_files():
+    """
+    Cleans up temporary files created by the backend.
+
+    Returns:
+        dict: A message indicating the cleanup was successful.
+    """
+    print("Received request to clean up temporary files!")
+
     try:
         pdf_model_instance.clean_up()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+    return {"message": "Temporary files cleaned up successfully"}
