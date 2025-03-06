da15fc357e166593a19de8f5ed359bb93bc35cf2
Andre Himmelspach <andre.himmelspach@tapdo.io>
2025-03-06 10:35:10 +0100

    Revert "feat: enable dynamic api path"
    This reverts commit 6478fb90f11102a76c59d71aa161355e7ce64d44.

--- frontend/src/api/myApi.ts
+++ frontend/src/api/myApi.ts
@@ -1,12 +1,7 @@
 // Create connection to the backend API
 import { Configuration, DefaultApi } from "./index";
 
-// Use environment variable or fallback to current domain
-const basePath =
-  process.env.REACT_APP_API_BASE_PATH ||
-  `${window.location.protocol}//${window.location.hostname}:${8000}`;
-
-const config = new Configuration({ basePath: basePath });
+const config = new Configuration({ basePath: "http://localhost:8000" });
 // Delete the User-Agent header, to not reveal sensitive information
 delete config.baseOptions.headers["User-Agent"];
 const api = new DefaultApi(config);
